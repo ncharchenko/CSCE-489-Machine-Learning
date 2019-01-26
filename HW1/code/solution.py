30acb132f9bc127218f3ff47d3aa2582c0fd1fc4
ncharchenko <ncharchenko@tamu.edu>
2019-01-26 17:04:44 -0600

    Made progress on show_images, need to get both images to show.

--- HW1/code/solution.py
+++ HW1/code/solution.py
@@ -4,138 +4,141 @@ from helper import *
 
 
 def show_images(data):
-	"""Show the input images and save them.
+        """Show the input images and save them.
 
-	Args:
-		data: A stack of two images from train data with shape (2, 16, 16).
-			  Each of the image has the shape (16, 16)
+        Args:
+                data: A stack of two images from train data with shape (2, 16, 16).
+                          Each of the image has the shape (16, 16)
 
-	Returns:
-		Do not return any arguments. Save the plots to 'image_1.*' and 'image_2.*' and
-		include them in your report
-	"""
-	### YOUR CODE HERE
-	
-	### END YOUR CODE
+        Returns:
+                Do not return any arguments. Save the plots to 'image_1.*' and 'image_2.*' and
+                include them in your report
+        """
+        ### YOUR CODE HERE
+        plt.imshow(data[0])
+        plt.imsave('image_1.png',data[0])
+        plt.imshow(data[1])
+        plt.imsave('image_2.png',data[1])
+        ### END YOUR CODE
 
 
 def show_features(X, y, save=True):
-	"""Plot a 2-D scatter plot in the feature space and save it. 
+        """Plot a 2-D scatter plot in the feature space and save it. 
 
-	Args:
-		X: An array of shape [n_samples, n_features].
-		y: An array of shape [n_samples,]. Only contains 1 or -1.
-		save: Boolean. The function will save the figure only if save is True.
+        Args:
+                X: An array of shape [n_samples, n_features].
+                y: An array of shape [n_samples,]. Only contains 1 or -1.
+                save: Boolean. The function will save the figure only if save is True.
 
-	Returns:
-		Do not return any arguments. Save the plot to 'train_features.*' and include it
-		in your report.
-	"""
-	### YOUR CODE HERE
-	plt.plot(X,y,'o',color='black')
-	plt.show()
-	plt.savefig('train_features.png')
-	### END YOUR CODE
+        Returns:
+                Do not return any arguments. Save the plot to 'train_features.*' and include it
+                in your report.
+        """
+        ### YOUR CODE HERE
+        plt.scatter(X,y,c=y)
+        plt.show()
+        plt.savefig('train_features.png')
+        ### END YOUR CODE
 
 
 class Perceptron(object):
-	
-	def __init__(self, max_iter):
-		self.max_iter = max_iter
+        
+        def __init__(self, max_iter):
+                self.max_iter = max_iter
 
-	def fit(self, X, y):
-		"""Train perceptron model on data (X,y).
+        def fit(self, X, y):
+                """Train perceptron model on data (X,y).
 
-		Args:
-			X: An array of shape [n_samples, n_features].
-			y: An array of shape [n_samples,]. Only contains 1 or -1.
+                Args:
+                        X: An array of shape [n_samples, n_features].
+                        y: An array of shape [n_samples,]. Only contains 1 or -1.
 
-		Returns:
-			self: Returns an instance of self.
-		"""
-		### YOUR CODE HERE
-		
-
-
-		### END YOUR CODE
-		
-		return self
-
-	def get_params(self):
-		"""Get parameters for this perceptron model.
-
-		Returns:
-			W: An array of shape [n_features,].
-		"""
-		if self.W is None:
-			print("Run fit first!")
-			sys.exit(-1)
-		return self.W
-
-	def predict(self, X):
-		"""Predict class labels for samples in X.
-
-		Args:
-			X: An array of shape [n_samples, n_features].
-
-		Returns:
-			preds: An array of shape [n_samples,]. Only contains 1 or -1.
-		"""
-		### YOUR CODE HERE
+                Returns:
+                        self: Returns an instance of self.
+                """
+                ### YOUR CODE HERE
+                
 
 
+                ### END YOUR CODE
+                
+                return self
 
-		### END YOUR CODE
+        def get_params(self):
+                """Get parameters for this perceptron model.
 
-	def score(self, X, y):
-		"""Returns the mean accuracy on the given test data and labels.
+                Returns:
+                        W: An array of shape [n_features,].
+                """
+                if self.W is None:
+                        print("Run fit first!")
+                        sys.exit(-1)
+                return self.W
 
-		Args:
-			X: An array of shape [n_samples, n_features].
-			y: An array of shape [n_samples,]. Only contains 1 or -1.
+        def predict(self, X):
+                """Predict class labels for samples in X.
 
-		Returns:
-			score: An float. Mean accuracy of self.predict(X) wrt. y.
-		"""
-		### YOUR CODE HERE
+                Args:
+                        X: An array of shape [n_samples, n_features].
+
+                Returns:
+                        preds: An array of shape [n_samples,]. Only contains 1 or -1.
+                """
+                ### YOUR CODE HERE
 
 
 
-		### END YOUR CODE
+                ### END YOUR CODE
+
+        def score(self, X, y):
+                """Returns the mean accuracy on the given test data and labels.
+
+                Args:
+                        X: An array of shape [n_samples, n_features].
+                        y: An array of shape [n_samples,]. Only contains 1 or -1.
+
+                Returns:
+                        score: An float. Mean accuracy of self.predict(X) wrt. y.
+                """
+                ### YOUR CODE HERE
+
+
+
+                ### END YOUR CODE
 
 
 
 
 def show_result(X, y, W):
-	"""Plot the linear model after training. 
-	   You can call show_features with 'save' being False for convenience.
+        """Plot the linear model after training. 
+           You can call show_features with 'save' being False for convenience.
 
-	Args:
-		X: An array of shape [n_samples, 2].
-		y: An array of shape [n_samples,]. Only contains 1 or -1.
-		W: An array of shape [n_features,].
-	
-	Returns:
-		Do not return any arguments. Save the plot to 'result.*' and include it
-		in your report.
-	"""
-	### YOUR CODE HERE
+        Args:
+                X: An array of shape [n_samples, 2].
+                y: An array of shape [n_samples,]. Only contains 1 or -1.
+                W: An array of shape [n_features,].
+        
+        Returns:
+                Do not return any arguments. Save the plot to 'result.*' and include it
+                in your report.
+        """
+        ### YOUR CODE HERE
 
 
 
-	### END YOUR CODE
+        ### END YOUR CODE
 
 
 
 def test_perceptron(max_iter, X_train, y_train, X_test, y_test):
 
-	# train perceptron
-	model = Perceptron(max_iter)
-	model.fit(X_train, y_train)
-	train_acc = model.score(X_train, y_train)
-	W = model.get_params()
+        # train perceptron
+        model = Perceptron(max_iter)
+        model.fit(X_train, y_train)
+        train_acc = model.score(X_train, y_train)
+        W = model.get_params()
 
-	# test perceptron model
-	test_acc = model.score(X_test, y_test)
+        # test perceptron model
+        test_acc = model.score(X_test, y_test)
 
-	return W, train_acc, test_acc
+        return W, train_acc, test_acc
